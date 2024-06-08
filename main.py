7f2b241a27a134cf028529242f8d53e888b9729f
fuantomu <fuantomuw@gmail.com>
2024-06-08 12:02:23 +0200

    fixed output

--- main.py
+++ main.py
@@ -51,24 +51,24 @@ def get_sorted_equipment(characterName):
     affixes = requests.get("https://raw.githubusercontent.com/fuantomu/envy-armory/main/affix.json").json()
     sortedEquipment = {
                     "HEAD": {}, 
-                    "NECK": 1,
-                    "SHOULDER": 2,
-                    "SHIRT": 3,
-                    "CHEST": 4,
-                    "WAIST": 5,
-                    "LEGS": 6,
-                    "FEET": 7,
-                    "WRIST": 8,
-                    "HANDS": 9,
-                    "FINGER_1": 10,
-                    "FINGER_2": 11,
-                    "TRINKET_1": 12,
-                    "TRINKET_2": 13,
-                    "BACK": 14,
-                    "MAIN_HAND": 15,
-                    "OFF_HAND": 16,
-                    "RANGED": 17,
-                    "TABARD": 18 
+                    "NECK": {},
+                    "SHOULDER": {},
+                    "SHIRT": {},
+                    "CHEST": {},
+                    "WAIST": {},
+                    "LEGS": {},
+                    "FEET": {},
+                    "WRIST": {},
+                    "HANDS": {},
+                    "FINGER_1": {},
+                    "FINGER_2": {},
+                    "TRINKET_1": {},
+                    "TRINKET_2": {},
+                    "BACK": {},
+                    "MAIN_HAND": {},
+                    "OFF_HAND": {},
+                    "RANGED": {},
+                    "TABARD": {} 
     }
     items = character["equipped_items"]
     for item in items:
@@ -101,7 +101,6 @@ def get_sorted_equipment(characterName):
                             if len(filtered) > 0:
                                 item["link"] += "&rand="
                             affixNames = [affixStats[entry.get("enchantment_id")] for entry in filtered]
-
                             for affix in affixes[item["inventory_type"]["type"]]["affix"]:
                                 if all([stat in affixes[item["inventory_type"]["type"]]["affix"][affix]["stats"] for stat in affixNames]):
                                     item["link"] += str(affix)
@@ -110,9 +109,10 @@ def get_sorted_equipment(characterName):
             if item.get("set"):
                 item["link"] += "&pcs="
                 item["link"] += ":".join([str(item["item"]["id"]) for item in items])
-                    
-            item["link"] = item["link"][1:]
-            sortedEquipment[item["slot"]["type"]] = item             
+            
+            if len(item["link"]) > 0:
+                item["link"] = item["link"][1:]
+        sortedEquipment[item["slot"]["type"]] = item        
     return sortedEquipment
 
 
